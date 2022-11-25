415fccb474c6b3f5b42bb3af256855a9998a53e2
svetanguyen <matematicaforeber@gmail.com>
2022-11-24 23:18:45 -0500

    refactor menulinks

--- src/components/MenuLinks.vue
+++ src/components/MenuLinks.vue
@@ -1,73 +1,22 @@
 <template>
   <ul class="min-w-[150px]">
-    <li>
+    <li v-for="(link, index) in menuLinks" :key="index">
       <router-link
         class="py-4 px-4 text-[23px] lg:text-xl leading-none flex gap-3 items-center text-black lg:text-[23px] hover:bg-dark-pink w-full"
         :to="{
           name: 'Home',
-          query: { max: $route.query.max ? 'about' : '', open: 'about' },
+          query: { max: $route.query.max ? link.query : '', open: link.query },
         }"
       >
         <img
           class="w-7"
-          :src="require(`../assets/images/heart.png`)"
-          alt="document"
+          :src="require(`../assets/images/${link.img}`)"
+          :alt="link.alt"
           width="30"
           height="30"
         />
-        <span>About</span>
+        <span>{{link.title}}</span>
       </router-link>
-    </li>
-    <li>
-      <router-link
-        class="py-4 px-4 text-[23px] lg:text-xl leading-none flex gap-3 items-center text-black lg:text-[23px] hover:bg-dark-pink w-full"
-        :to="{
-          name: 'Home',
-          query: { max: $route.query.max ? 'works' : '', open: 'works' },
-        }"
-      >
-        <img
-          class="w-7"
-          :src="require(`../assets/images/folder.png`)"
-          alt="folder"
-          width="30"
-          height="30"
-        />
-        Works</router-link
-      >
-    </li>
-    <li>
-      <router-link
-        class="p-4 text-[23px] lg:text-xl leading-none flex gap-3 items-center text-black lg:text-[23px] hover:bg-dark-pink w-full"
-        :to="{
-          name: 'Home',
-          query: { max: $route.query.max ? 'contact' : '', open: 'contact' },
-        }"
-      >
-        <img
-          class="w-7"
-          :src="require(`../assets/images/files.png`)"
-          alt="files"
-          width="30"
-          height="30"
-        />
-        Contacts</router-link
-      >
-    </li>
-    <li>
-      <router-link
-        class="p-4 text-[23px] lg:text-xl leading-none flex items-center text-black lg:text-[23px] hover:bg-dark-pink w-full"
-        :to="{ name: 'Home', query: { max: '', open: 'calculator' } }"
-      >
-        <img
-          class="w-7"
-          :src="require(`../assets/images/calculator.png`)"
-          alt="calculator"
-          width="30"
-          height="30"
-        />
-        <span class="ml-3 block">Calculator</span></router-link
-      >
     </li>
   </ul>
 </template>
@@ -79,6 +28,32 @@ export default {
   data() {
     return {
       route: this.$route,
+      menuLinks: [
+        {
+          query: 'about',
+          img: 'heart.png',
+          alt: 'heart',
+          title: 'About'
+        },
+        {
+          query: 'works',
+          img: 'folder.png',
+          alt: 'folder',
+          title: 'Works'
+        },
+        {
+          query: 'contact',
+          img: 'files.png',
+          alt: 'files',
+          title: 'Contacts'
+        },
+        {
+          query: 'calculator',
+          img: 'calculator.png',
+          alt: 'calculator',
+          title: 'Calculator'
+        },
+      ]
     };
   },
 };
